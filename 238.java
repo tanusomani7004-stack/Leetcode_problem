16b6fc5ba0b98d6439c97ba8a8e2be3060618cac
Tanu <tanusomani7004@gmail.com>
2026-09-01 15:37:12 +0530

    Create 238.java

---
+++ 238.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n= nums.length;
+        int[] pre =new int[n];
+        int[] post=new int[n];
+        int[] res=new int[n];
+
+        pre[0] =nums[0];
+        post[n-1]=nums[n-1];
+
+        for (int i=1;i<n;i++){
+        pre[i]= nums[i]*pre[i-1];    
+}
+
+        for(int i=n-2; i>=0;i--){
+            post[i]=nums[i]*post[i+1];
+        }
+        res[0]=post[1];
+        res[n-1]=pre[n-2];
+
+        for(int i =1; i<n-1; i++){
+            res[i] = pre[i-1]*post[i+1];
+        }
+        return res;
+
+}
+}
